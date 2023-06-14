2496e5e546fa8d6e644b79668124ea1587837e84
DENISHKUMAR CHOVATIYA <53310560+dgchovatiya@users.noreply.github.com>
2023-06-14 10:06:19 -0500

    Cloud project

---
+++ README.md
@@ -0,0 +1,11 @@
+## Team LLC
+
+
+You enter a 'Dataset Name' (think 'parent folder') and a 'label'  and all images captured will appear in s3://<your_bucket_name_described_in_src/aws_exports.js>/public/<dataset_name>/
+
+## Setup
+
+Please configure aws amilfy in your desktop and run these commands `amplify init`, then `amplify storage add` and then then `amplify push`.
+and then run react project using 'npm start' it will run the app.
+
+Please refer video of our project which shows how our project works.
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cloudproject07013508": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "cloudproject": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
